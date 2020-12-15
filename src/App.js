15c3699842c84903f4641acceefd44ec8958f9c3
mirthindra <mirthindra@gmail.com>
2020-12-15 10:41:58 +0900

    create react project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-color">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and build Manage System
         </a>
       </header>
     </div>
